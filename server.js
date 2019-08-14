58a75c09023fe2bc5696f5823d5459f8d512d1aa
Tim Shim <timshim@gmail.com>
2019-08-14 14:44:55 +0800

    Created basic app to server products with pagination and get product details

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+app.get('/products', (req, res) => {
+  let page = req.query.page;
+  let products = [];
+
+  if (!page || page <= 0) {
+    page = 1;
+  }
+
+  const startingItem = (page * 12) - 11;
+  const lastItem = page * 12;
+
+  for (i = startingItem; i <= lastItem; i++) {
+    const product = {
+      id: i,
+      name: `Sample Product ${i}`,
+      description: `This is a sample description for product ${i}`,
+      price: 9.99,
+      thumbnailUrl: `https://picsum.photos/id/${i}/200/120`
+    }
+    products.push(product);
+  }
+
+  res.json({ products });
+});
+
+app.get('/product/:id', (req, res) => {
+  const id = req.params.id;
+  const product = {
+    id: parseInt(id),
+    name: `Sample Product ${id}`,
+    description: `This is a sample description for product ${id}`,
+    price: 9.99,
+    imageUrl: `https://picsum.photos/id/${id}/400/240`,
+  }
+  res.json({ product });
+});
